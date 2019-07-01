23735d959763c8f00d6785c0651ded735a37a6d0
DavidRimon <davidthe16@gmail.com>
2019-07-02 01:41:39 +0300

    Update git-alias.sh

--- git-alias.sh
+++ git-alias.sh
@@ -2,7 +2,8 @@
 git config --global alias.morgen init
 git config --global alias.shabbos push
 git config --global alias.vach pull
-git config --global alias.vort commit
+git config --global alias.vort log
 git config --global alias.yomtev checkout
 git config --global alias.kvitell status
 git config --global alias.adunk add
+git config --global alias.shkoiech commit
