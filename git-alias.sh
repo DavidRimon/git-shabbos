ec340944c035762e1566907e664766357be0be0c
DavidRimon <davidthe16@gmail.com>
2019-07-02 01:03:51 +0300

    Create git-alias.sh

---
+++ git-alias.sh
@@ -0,0 +1,8 @@
+#/bin/sh
+git config --global alias.morgen init
+git config --global alias.shabbos push
+git config --global alias.vach pull
+git config --global alias.vort commit
+git config --global alias.yomtev checkout
+git config --global alias.kvitell status
+git config --global alias.adunk add
